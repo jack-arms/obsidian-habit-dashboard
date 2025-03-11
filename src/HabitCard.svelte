9d165cb3172e011c78b59107f7a9db272f174708
Jack Armstrong <jackarms@meta.com>
2025-03-10 20:53:51 -0700

    switch to accordion component for habit card

--- src/HabitCard.svelte
+++ src/HabitCard.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
   import type { Habit } from "./main";
-  import {
-    Flag,
-    Pencil,
-    History,
-    Hourglass,
-    ChevronDown,
-    ChevronUp,
-  } from "lucide-svelte";
-  import { Button, Card } from "flowbite-svelte";
+  import { Flag, Pencil, History, Hourglass } from "lucide-svelte";
+  import { Accordion, AccordionItem, Button } from "flowbite-svelte";
   import {
     daysBetween,
     goalIntervalTimeUnitToString,
@@ -24,7 +17,6 @@
   }
 
   let { habit, onEdit, habitProgress }: Props = $props();
-  let expanded = $state(false);
   let daysSince = $derived(
     daysBetween(
       new Date(),
@@ -33,71 +25,57 @@
   );
 </script>
 
-<Card class="p-0!">
-  <div
-    class="flex flex-row items-center p-2 {expanded
-      ? 'shadow-md! transition-shadow duration-0 delay-0'
-      : 'shdow-none! transition-shadow duration-0 delay-500'}"
+<Accordion
+  class="accordion border border-gray-200 dark:border-gray-700 rounded-md"
+  flush
+>
+  <AccordionItem
+    class="p-4! h-auto! border-b! border-gray-200! dark:border-gray-700! bg-gray-50! hover:bg-gray-100!"
+    tag="div"
   >
-    <h5 class="mb-0! mt-0! text-2xl font-bold text-gray-900 flex-grow">
-      {habit.name}
-    </h5>
-    <Hourglass />
-    {daysSince}
-    {#if expanded}
-      <Button
-        outline={true}
-        class="shadow-none! p-2! focus:ring-0"
-        on:click={() => (expanded = false)}
-      >
-        <ChevronUp />
-      </Button>
-    {:else}
-      <Button
-        outline={true}
-        class="shadow-none! p-2! focus:ring-0"
-        on:click={() => (expanded = true)}
-      >
-        <ChevronDown />
-      </Button>
-    {/if}
-  </div>
-  <div
-    class="flex flex-col {!expanded
-      ? 'max-h-0'
-      : 'max-h-50'} transition-max-height duration-500 ease-in-out overflow-hidden"
-  >
-    <div class="flex flex-row p-2">
-      <div class="flex flex-row items-center grow">
-        <History class="mr-2" />
-        {#if Object.keys(habitProgress).length === 0}
-          Never done!
-        {:else}
-          Last done on {Object.values(habitProgress)[
-            Object.keys(habitProgress).length - 1
-          ].date}
-        {/if}
-      </div>
-      <div class="flex justify-end p-2 mr-auto">
-        <Button
-          outline={true}
-          class="shadow-none! p-2!"
-          on:click={() => onEdit()}
-        >
-          <Pencil class="w-5 h-5" />
-        </Button>
-      </div>
+    <div slot="header" class="flex flex-row flex-grow items-center pr-2">
+      <h5 class="mb-0! mt-0! text-2xl font-bold text-gray-900 flex-grow">
+        {habit.name}
+      </h5>
+      <Hourglass />
+      {daysSince}
     </div>
-    {#if habit.goalInfo != null}
-      <span>
-        <Flag class="inline mr-2" />{habit.goalInfo.goal}
-        {goalTimeUnitToString(habit.goalInfo.goalTimeUnit, habit.goalInfo.goal)}
-        every {habit.goalInfo.interval}
-        {goalIntervalTimeUnitToString(
-          habit.goalInfo.intervalTimeUnit,
-          habit.goalInfo.interval,
-        )}
-      </span>
-    {/if}
-  </div>
-</Card>
+    <div>
+      <div class="flex flex-row p-2">
+        <div class="flex flex-row items-center grow">
+          <History class="mr-2" />
+          {#if Object.keys(habitProgress).length === 0}
+            Never done!
+          {:else}
+            Last done on {Object.values(habitProgress)[
+              Object.keys(habitProgress).length - 1
+            ].date}
+          {/if}
+        </div>
+        <div class="flex justify-end p-2 mr-auto">
+          <Button
+            outline={true}
+            class="shadow-none! p-2!"
+            on:click={() => onEdit()}
+          >
+            <Pencil class="w-5 h-5" />
+          </Button>
+        </div>
+      </div>
+      {#if habit.goalInfo != null}
+        <span>
+          <Flag class="inline mr-2" />{habit.goalInfo.goal}
+          {goalTimeUnitToString(
+            habit.goalInfo.goalTimeUnit,
+            habit.goalInfo.goal,
+          )}
+          every {habit.goalInfo.interval}
+          {goalIntervalTimeUnitToString(
+            habit.goalInfo.intervalTimeUnit,
+            habit.goalInfo.interval,
+          )}
+        </span>
+      {/if}
+    </div>
+  </AccordionItem>
+</Accordion>
